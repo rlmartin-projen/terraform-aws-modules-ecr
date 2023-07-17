86f3ff5248a25230e5969030b29f599692ef157d
Ryan Martin <ryan@vestahealthcare.com>
2023-07-17 16:50:50 -0400

    chore: add NPM deployment

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,4 +1,9 @@
+import { NpmAccess } from "projen/lib/javascript";
 import { TerraformModule } from "projen-cdktf-hybrid-construct";
+
+const majorVersion = 2;
+const nodeVersion = "18";
+
 const project = new TerraformModule({
   author: "Ryan Martin",
   authorAddress: "rlmartin@gmail.com",
@@ -15,6 +20,19 @@ const project = new TerraformModule({
       version: "~> 1.0",
     },
   ],
+  releaseToNpm: true,
+  npmAccess: NpmAccess.PUBLIC,
+  minNodeVersion: `${nodeVersion}.0.0`,
+  workflowNodeVersion: nodeVersion,
+  majorVersion,
+  releaseBranches: {
+    dev: { prerelease: "dev", npmDistTag: "dev", majorVersion },
+  },
+  depsUpgradeOptions: {
+    workflowOptions: {
+      branches: ["main"],
+    },
+  },
 
   // cdktfVersion: "^0.13.0",      /* Minimum target version of this library. */
   // deps: [],                     /* Runtime dependencies of this module. */
