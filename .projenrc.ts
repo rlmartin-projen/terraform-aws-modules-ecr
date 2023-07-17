c75a4624cb0809fc6f74cc0a92b7a9eeb5cf5ad6
Ryan Martin <ryan@vestahealthcare.com>
2023-07-17 16:03:50 -0400

    chore: project created with projen

---
+++ .projenrc.ts
@@ -0,0 +1,18 @@
+import { TerraformModule } from "projen-cdktf-hybrid-construct";
+const project = new TerraformModule({
+  author: "Ryan Martin",
+  authorAddress: "ryan@vestahealthcare.com",
+  defaultReleaseBranch: "main",
+  devDeps: ["projen-cdktf-hybrid-construct"],
+  jsiiVersion: "~5.0.0",
+  name: "terraform-aws-modules-ecr",
+  projenrcTs: true,
+  repositoryUrl: "git@github.com:rlmartin-projen/terraform-aws-modules-ecr.git",
+
+  // cdktfVersion: "^0.13.0",      /* Minimum target version of this library. */
+  // deps: [],                     /* Runtime dependencies of this module. */
+  // description: undefined,       /* The description is just a string that helps people understand the purpose of the package. */
+  // packageName: undefined,       /* The "name" in package.json. */
+  // terraformModules: undefined,  /* List of modules to generate bindings for. */
+});
+project.synth();
